2a898007b81074cc1190cc7b3ba879cbc9ff8263
walkerdn489 <walkerdn2020@gmail.com>
2022-07-03 18:10:40 -0700

    updates

--- DataBaseHelperFunctionsTestFixture.py
+++ DataBaseHelperFunctionsTestFixture.py
@@ -82,16 +82,18 @@ class TestGetLonLatFromZip(unittest.TestCase):
 class TestDateTimeConversions(unittest.TestCase):
     helper = databaseHelpers()
 
+
     def test_TimeConversions(self):
         date = "05/18/1998"
         time = self.helper.convertDateToTime(date)
-
+ 
         #This is based on time zone when running
         self.assertEqual(time, 895449600.0,"Time should be around 895449600.0 seconds")
         
         # May 18th 1998
         date = self.helper.convertTimeToDate(time)
         date = date.strftime('%m/%d/%Y')
+
         month = date[0] + date[1]
         day = date[3] + date[4]
         year = date[6] + date[7] + date[8] + date[9]
